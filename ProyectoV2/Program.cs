dfd6fff818614182671ac4865622e602d1d4c832
zertumez-admin <zertumez.art@gmail.com>
2023-04-23 10:38:51 -0600

    Functions for parsing and detecting errrors in input where added

--- ProyectoV2/Program.cs
+++ ProyectoV2/Program.cs
@@ -5,9 +5,114 @@ namespace Calculadora
 {
     public static class SepararParentesis
     {
+    public static string ParseText(string input, List<char> caracteresAceptados)
+            {
+                // Remove any empty characters from the input string
+                // Remove any empty characters from the input string
+                input = input.Replace(" ", "");
+                input = input.Replace("\t", "");
+                input = input.Replace("\n", "");
+                input = input.Replace("\r", "");
+                Console.WriteLine($"Ejercicio sin espacios: {input}\n");
+
+                // We can use LINQ to filter out any characters that are not in the caracteresAceptados list
+                string output = new string(input.Where(c => caracteresAceptados.Contains(c)).ToArray());
+
+                if (output == "")
+                {
+                    throw new Exception("La input contiene carácteres vacíos ó no contiene caracteres aceptados.");
+                }
+
+                return output;
+            }
+    // Creamos una función para revisar que la variable ejercicio no contenga:
+    // más de 1 asterisco enseguida a otro, mas de 1 diagonal enseguida de otra, 
+    // mas de 1 punto enseguida de otro, más de 1 exponente enseguida de otro, 
+    // mas de 1, si no hay un cero enseguida de una diagonal(para que no se divida entre cero) 
+    // y que cuide que no se mezcle ninguno de estos símbolos directamente entre sí
+    public static bool RevisarEjercicio(string ejercicio)
+    {
+        // Creamos una lista con los símbolos que no deben mezclarse directamente entre sí
+        List<string> simbolosNoMezclables = new List<string> { "*", "/", "^" };
+
+        // Creamos una lista con los símbolos que no deben aparecer más de una vez seguida de otra
+        List<string> simbolosNoRepetibles = new List<string> { "*", "/", ".", "^", "," };
+
+        // Creamos una variable para almacenar el último símbolo revisado
+        string ultimoSimbolo = "";
+
+        // Creamos una variable para almacenar la cantidad de veces que se ha repetido el último símbolo
+        int cantidadRepetida = 0;
+
+        // Recorremos cada símbolo del ejercicio
+        foreach (var simbolo in ejercicio)
+        {
+            // Si el símbolo es una diagonal y el último símbolo revisado fue un cero
+            if (simbolo == '/' && ultimoSimbolo.Equals("0"))
+            {
+                Console.WriteLine("El ejercicio contiene una división entre cero.");
+                return false;
+            }
+
+            // Si el símbolo es uno de los símbolos no mezclables y el último símbolo revisado también lo era
+            if (simbolosNoMezclables.Contains(simbolo.ToString()) && simbolosNoMezclables.Contains(ultimoSimbolo))
+            {
+                Console.WriteLine("El ejercicio contiene símbolos no mezclables directamente entre sí.");
+                return false;
+            }
+
+            // Si el símbolo es igual al último revisado
+            if (simbolo.ToString() == ultimoSimbolo)
+            {
+                cantidadRepetida++;
+
+                // Si la cantidad de veces que se ha repetido el símbolo es mayor a 1 y el símbolo no es un punto o una coma
+                if (cantidadRepetida > 1 && !simbolosNoRepetibles.Contains(simbolo.ToString()))
+                {
+                    Console.WriteLine($"El ejercicio contiene más de 1 símbolo {simbolo} seguido de otro.");
+                    return false;
+                }
+            }
+            else
+            {
+                // Si el símbolo es diferente al último revisado, actualizamos las variables
+                ultimoSimbolo = simbolo.ToString();
+                cantidadRepetida = 1;
+            }
+        }
+
+        // Si no se encontró ningún problema, el ejercicio es válido
+        return true;
+    }
+        
         public static void Main()
         {
-            string ejercicio = "4-2*((12-4)+3+1)";
+            // Example usage:
+            // List of accepted characters
+            List<char> caracteresAceptados = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')', '^', '.'};
+
+
+            // Input string
+            string ejercicio;
+            do{
+                Console.Write("Ingrese el ejercicio: ");
+                ejercicio = Console.ReadLine();
+            } 
+            while (string.IsNullOrEmpty(ejercicio));
+
+
+            // Call the ParseText function
+            ejercicio = ParseText(ejercicio, caracteresAceptados);
+            
+            
+            if (ejercicio.revisarEjercicio(ejercicio) == false)
+            {
+                return;
+            }
+
+            // Output should be "abcde"
+            Console.WriteLine(ejercicio);
+
             Console.WriteLine($"Ejercicio inicial: {ejercicio}\n");
 
             // Creamos una nueva lista para almacenar los pasos del proceso
@@ -35,7 +140,7 @@ namespace Calculadora
             }
 
             //Que el ejercicio no tenga más de 6 pares de parentesis
-            if (countParentesisIzq > 6)
+            if (countParentesisIzq > 12)
             {
                 Console.WriteLine("El ejercicio tiene más de 6 pares de paréntesis.");
                 return;
