47038d6264423d32d72ff0826ef66975596604ec
Miguel M <nanopizzafuturo@gmail.com>
2023-04-24 00:55:44 -0600

    New functions were added

--- ProyectoV2/Program.cs
+++ ProyectoV2/Program.cs
@@ -5,110 +5,119 @@ namespace Calculadora
 {
     public static class SepararParentesis
     {
-    public static string ParseText(string input, List<char> caracteresAceptados)
-            {
-                // Remove any empty characters from the input string
-                // Remove any empty characters from the input string
-                input = input.Replace(" ", "");
-                input = input.Replace("\t", "");
-                input = input.Replace("\n", "");
-                input = input.Replace("\r", "");
-                Console.WriteLine($"Ejercicio sin espacios: {input}\n");
-
-                // We can use LINQ to filter out any characters that are not in the caracteresAceptados list
-                string output = new string(input.Where(c => caracteresAceptados.Contains(c)).ToArray());
-
-                if (output == "")
-                {
-                    throw new Exception("La input contiene carácteres vacíos ó no contiene caracteres aceptados.");
-                }
-
-                return output;
-            }
-    // Creamos una función para revisar que la variable ejercicio no contenga:
-    // más de 1 asterisco enseguida a otro, mas de 1 diagonal enseguida de otra, 
-    // mas de 1 punto enseguida de otro, más de 1 exponente enseguida de otro, 
-    // mas de 1, si no hay un cero enseguida de una diagonal(para que no se divida entre cero) 
-    // y que cuide que no se mezcle ninguno de estos símbolos directamente entre sí
-    public static bool RevisarEjercicio(string ejercicio)
-    {
-        // Creamos una lista con los símbolos que no deben mezclarse directamente entre sí
-        List<string> simbolosNoMezclables = new List<string> { "*", "/", "^" };
-
-        // Creamos una lista con los símbolos que no deben aparecer más de una vez seguida de otra
-        List<string> simbolosNoRepetibles = new List<string> { "*", "/", ".", "^", "," };
-
-        // Creamos una variable para almacenar el último símbolo revisado
-        string ultimoSimbolo = "";
-
-        // Creamos una variable para almacenar la cantidad de veces que se ha repetido el último símbolo
-        int cantidadRepetida = 0;
-
-        // Recorremos cada símbolo del ejercicio
-        foreach (var simbolo in ejercicio)
+        public static string ParseText(string input, List<char> caracteresAceptados)
         {
-            // Si el símbolo es una diagonal y el último símbolo revisado fue un cero
-            if (simbolo == '/' && ultimoSimbolo.Equals("0"))
+            // Remove any empty characters from the input string
+            // Remove any empty characters from the input string
+            input = input.Replace(" ", "");
+            input = input.Replace("\t", "");
+            input = input.Replace("\n", "");
+            input = input.Replace("\r", "");
+            Console.WriteLine($"Ejercicio sin espacios: {input}\n");
+
+            // We can use LINQ to filter out any characters that are not in the caracteresAceptados list
+            string output = new string(input.Where(c => caracteresAceptados.Contains(c)).ToArray());
+
+            if (output == "")
             {
-                Console.WriteLine("El ejercicio contiene una división entre cero.");
-                return false;
+                throw new Exception("La input contiene carácteres vacíos ó no contiene caracteres aceptados.");
             }
 
-            // Si el símbolo es uno de los símbolos no mezclables y el último símbolo revisado también lo era
-            if (simbolosNoMezclables.Contains(simbolo.ToString()) && simbolosNoMezclables.Contains(ultimoSimbolo))
-            {
-                Console.WriteLine("El ejercicio contiene símbolos no mezclables directamente entre sí.");
-                return false;
-            }
+            return output;
+        }
+        // Creamos una función para revisar que la variable ejercicio no contenga:
+        // más de 1 asterisco enseguida a otro, mas de 1 diagonal enseguida de otra, 
+        // mas de 1 punto enseguida de otro, más de 1 exponente enseguida de otro, 
+        // mas de 1, si no hay un cero enseguida de una diagonal(para que no se divida entre cero) 
+        // y que cuide que no se mezcle ninguno de estos símbolos directamente entre sí
+        public static bool RevisarEjercicio(string ejercicio)
+        {
+            // Creamos una lista con los símbolos que no deben mezclarse directamente entre sí
+            List<string> simbolosNoMezclables = new List<string> { "*", "/", "^" };
 
-            // Si el símbolo es igual al último revisado
-            if (simbolo.ToString() == ultimoSimbolo)
-            {
-                cantidadRepetida++;
+            // Creamos una lista con los símbolos que no deben aparecer más de una vez seguida de otra
+            List<string> simbolosNoRepetibles = new List<string> { "*", "/", ".", "^", "," };
 
-                // Si la cantidad de veces que se ha repetido el símbolo es mayor a 1 y el símbolo no es un punto o una coma
-                if (cantidadRepetida > 1 && !simbolosNoRepetibles.Contains(simbolo.ToString()))
+            // Creamos una variable para almacenar el último símbolo revisado
+            string ultimoSimbolo = "";
+
+            // Creamos una variable para almacenar la cantidad de veces que se ha repetido el último símbolo
+            int cantidadRepetida = 0;
+
+            // Recorremos cada símbolo del ejercicio
+            foreach (var simbolo in ejercicio)
+            {
+                // Si el símbolo es una diagonal y el último símbolo revisado fue un cero
+                if (simbolo == '/' && ultimoSimbolo.Equals("0"))
                 {
-                    Console.WriteLine($"El ejercicio contiene más de 1 símbolo {simbolo} seguido de otro.");
+                    Console.WriteLine("El ejercicio contiene una división entre cero.");
                     return false;
                 }
+
+                // Si el símbolo es uno de los símbolos no mezclables y el último símbolo revisado también lo era
+                if (simbolosNoMezclables.Contains(simbolo.ToString()) && simbolosNoMezclables.Contains(ultimoSimbolo))
+                {
+                    Console.WriteLine("El ejercicio contiene símbolos no mezclables directamente entre sí.");
+                    return false;
+                }
+
+                // Si el símbolo es igual al último revisado
+                if (simbolo.ToString() == ultimoSimbolo)
+                {
+                    // Si el símbolo es un paréntesis y el último símbolo también es un paréntesis, continuar la iteración
+                    if ((simbolo == ')' && ultimoSimbolo == "(") || (simbolo == '(' && ultimoSimbolo == ")") || (simbolo == '(' && ultimoSimbolo == "(") || (simbolo == ')' && ultimoSimbolo == ")"))
+                    {
+                        continue;
+                    }
+
+                    cantidadRepetida++;
+
+                    // Si la cantidad de veces que se ha repetido el símbolo es mayor a 1 y el símbolo no es un punto o una coma
+                    if (cantidadRepetida > 1 && !simbolosNoRepetibles.Contains(simbolo.ToString()))
+                    {
+                        Console.WriteLine($"El ejercicio contiene más de 1 símbolo {simbolo} seguido de otro.");
+                        return false;
+                    }
+                }
+                else
+                {
+                    // Si el símbolo es diferente al último revisado, actualizamos las variables
+                    ultimoSimbolo = simbolo.ToString();
+                    cantidadRepetida = 1;
+                }
             }
-            else
-            {
-                // Si el símbolo es diferente al último revisado, actualizamos las variables
-                ultimoSimbolo = simbolo.ToString();
-                cantidadRepetida = 1;
-            }
+
+            // Si no se encontró ningún problema, el ejercicio es válido
+            return true;
         }
 
-        // Si no se encontró ningún problema, el ejercicio es válido
-        return true;
-    }
-        
+
         public static void Main()
         {
             // Example usage:
             // List of accepted characters
-            List<char> caracteresAceptados = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')', '^', '.'};
+            List<char> caracteresAceptados = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')', '^', '.' };
 
 
             // Input string
             string ejercicio;
-            do{
+            do
+            {
                 Console.Write("Ingrese el ejercicio: ");
                 ejercicio = Console.ReadLine();
-            } 
+            }
             while (string.IsNullOrEmpty(ejercicio));
 
 
             // Call the ParseText function
             ejercicio = ParseText(ejercicio, caracteresAceptados);
-            
-            
-            if (ejercicio.revisarEjercicio(ejercicio) == false)
+
+
+            if (SepararParentesis.RevisarEjercicio(ejercicio) == false)
             {
                 return;
             }
+
 
             // Output should be "abcde"
             Console.WriteLine(ejercicio);
@@ -163,7 +172,7 @@ namespace Calculadora
 
                 // Se reemplaza el contenido de los paréntesis por dentroParentesis en el string del ejercicio
                 ejercicio = ejercicio.Remove(inicio, fin - inicio + 1);
-                
+
                 Console.WriteLine($"Se removió el siguiente string: {dentroParentesis}");
                 Console.WriteLine($"Ejercicio actual: {ejercicio}\n");
             }
